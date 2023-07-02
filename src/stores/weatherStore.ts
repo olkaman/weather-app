194763c2d87ce82760fa6efd14475dfb9a679e96
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2023-07-02 11:41:20 +0200

    Add latitude and longitude to store

--- src/stores/weatherStore.ts
+++ src/stores/weatherStore.ts
@@ -4,13 +4,22 @@ import { devtools } from 'zustand/middleware'
 
 type CurrentWeatherState = {
   currentWeatherData: CurrentWeatherData
+  latitude: string
+  longitude: string
+  location: string
 }
 
 type CurrentWeatherActions = {
   setCurrentWeather: (currentWeather: CurrentWeatherData) => void
+  setLatitude: (latidude: string) => void
+  setLongitude: (longitude: string) => void
+  setLocation: (location: string) => void
 }
 
 const initialState = {
+  latitude: '',
+  longitude: '',
+  location: '',
   currentWeatherData: {
     temperature: 0,
     name: ' ',
@@ -31,13 +40,37 @@ export const useWeatherStore = create<CurrentWeatherState & CurrentWeatherAction
   devtools((set) => ({
     ...initialState,
     setCurrentWeather: (currentWeather: CurrentWeatherData) => {
-      set(setCurrentWeather(currentWeather))
+      set(() => ({ currentWeatherData: currentWeather }), false, 'Set current weather data')
+    },
+    setLatitude: (latitude: string) => {
+      set(() => ({ latitude }), false, 'Set latitude')
+    },
+    setLongitude: (longitude: string) => {
+      set(() => ({ longitude }), false, 'Set longitude')
+    },
+    setLocation: (location: string) => {
+      set(() => ({ location }), false, 'Set location')
     }
   }))
 )
 
 export const useCurrentWeatherData = () => useWeatherStore((state) => state.currentWeatherData)
+export const useLatitude = () => useWeatherStore((state) => state.latitude)
+export const useLongitude = () => useWeatherStore((state) => state.longitude)
+export const useLocation = () => useWeatherStore((state) => state.location)
 
-function setCurrentWeather(currentWeather: CurrentWeatherData): (state: CurrentWeatherState) => CurrentWeatherState {
-  return (state) => ({ ...state, currentWeatherData: currentWeather })
-}
+// function setCurrentWeather(currentWeather: CurrentWeatherData): (state: CurrentWeatherState) => CurrentWeatherState {
+//   return (state) => ({ ...state, currentWeatherData: currentWeather })
+// }
+
+// function setLatitude(latitude: string): (state: CurrentWeatherState) => CurrentWeatherState {
+//   return (state) => ({ ...state, latitude })
+// }
+
+// function setLongitude(longitude: string): (state: CurrentWeatherState) => CurrentWeatherState {
+//   return (state) => ({ ...state, longitude })
+// }
+
+// function setLocation(location: string): (state: CurrentWeatherState) => CurrentWeatherState {
+//   return (state) => ({ ...state, location })
+// }
